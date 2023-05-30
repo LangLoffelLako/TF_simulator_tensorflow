fd501ef80bedde74c30c807dd2436c6d7376136b
LangLoffelLako <luis@mienhardt.de>
2023-05-30 12:48:24 +0200

    cluster bash script

--- hpc_cluster_cpu.sh
+++ hpc_cluster_cpu.sh
@@ -8,8 +8,8 @@
 module load cuda/11.8
 echo "Start at `date`"
 echo "---"
-source "C:\Users\Luis\miniconda3\envs\tf_architectur_simulation\lib\venv\scripts\common\activate"
-srun --nodes=2 python hpc_script_inference_test.py
+source "/home/<BENUTZERNAME>/workspace/venv/bin/activate"
+srun --nodes=2 python hpc_train_model.py
 deactivate
 echo "---"
 echo "Done at `date`"
